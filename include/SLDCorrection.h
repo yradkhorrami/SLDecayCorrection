dabbc8a211f2a615fe2e0c74f37f90b3c7b18930
Yasser Radkhorrami <yasser.radkhorrami@desy.de>
2021-08-02 20:18:01 +0200

    create SLDCorrection files

--- include/SLDCorrection.h
+++ include/SLDCorrection.h
@@ -1,2 +1,76 @@
 #ifndef SLDCorrection_h
 #define SLDCorrection_h 1
+#include <marlin/Processor.h>
+#include <marlin/Global.h>
+#include "EVENT/LCStrVec.h"
+#include <EVENT/MCParticle.h>
+#include <EVENT/Track.h>
+#include "IMPL/LCCollectionVec.h"
+#include "UTIL/LCRelationNavigator.h"
+#include <IMPL/ReconstructedParticleImpl.h>
+#include "lcio.h"
+#include <string>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <vector>
+#include "TLorentzVector.h"
+#include "TVector3.h"
+#include "TMatrixD.h"
+class TFile;
+class TDirectory;
+class TH1F;
+class TH1I;
+class TH2I;
+class TH2F;
+class TTree;
+class TF1;
+
+using namespace lcio ;
+using namespace marlin ;
+
+class SLDCorrection : public Processor
+{
+public:
+	virtual Processor *newProcessor()
+	{
+		return new SLDCorrection;
+	}
+	SLDCorrection();
+	virtual ~SLDCorrection() = default;
+	SLDCorrection( const SLDCorrection& ) = delete;
+	SLDCorrection &operator = ( const SLDCorrection& ) = delete;
+	virtual void init();
+	virtual void Clear();
+	virtual void processRunHeader();
+	virtual void processEvent( EVENT::LCEvent *pLCEvent );
+	virtual void check( EVENT::LCEvent *pLCEvent );
+	virtual void end();
+
+private:
+	typedef std::vector<int>			IntVector;
+	typedef std::vector<double>			DoubleVector;
+	typedef std::vector<float>			FloatVector;
+
+	std::string				m_mcParticleCollection{};
+	std::string				m_inputPfoCollection{};
+	std::string				m_inputJetCollection{};
+	std::string				m_jetFlavour{};
+	std::string				m_inputPrimaryVertex{};
+	std::string				m_inputBuildUpVertex{};
+	std::string				m_RecoMCTruthLinkCollection{};
+	std::string				m_MCTruthRecoLinkCollection{};
+	std::string				m_TrackMCTruthLinkCollection{};
+	std::string				m_MCTruthTrackLinkCollection{};
+	std::string				m_ClusterMCTruthLinkCollection{};
+	std::string				m_MCTruthClusterLinkCollection{};
+	std::string				m_SLDNuCollection{};
+	std::string				m_rootFile{};
+
+
+
+	TFile					*m_pTFile{};
+	TTree					*m_pTTree{};
+
+};
+#endif
--- src/SLDCorrection.cc
+++ src/SLDCorrection.cc
@@ -1,1 +1,163 @@
 #include "SLDCorrection.h"
+#include <iostream>
+#include <EVENT/LCCollection.h>
+#include "EVENT/LCCollection.h"
+#include "IMPL/LCCollectionVec.h"
+#include "EVENT/MCParticle.h"
+#include "EVENT/Vertex.h"
+#include "EVENT/ReconstructedParticle.h"
+#include <IMPL/ReconstructedParticleImpl.h>
+#include "IMPL/ParticleIDImpl.h"
+#include "UTIL/PIDHandler.h"
+#include "marlin/VerbosityLevels.h"
+#include <GeometryUtil.h>
+#include "TLorentzVector.h"
+#include "TVector3.h"
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TH1I.h"
+#include "TH2I.h"
+#include "TF1.h"
+#include "TTree.h"
+#include "TPaveStats.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TPad.h"
+#include "TRatioPlot.h"
+#include "TAxis.h"
+#include "TLine.h"
+
+using namespace lcio ;
+using namespace marlin ;
+
+SLDCorrection aSLDCorrection;
+
+SLDCorrection::SLDCorrection() :
+
+Processor("SLDCorrection")
+{
+	_description = "SLDCorrection finds semi-leptonic decays within jets and performs a correction to 4-momentum of the jet due to the missing neutrino(s)";
+
+	registerInputCollection(	LCIO::MCPARTICLE,
+					"MCParticleCollection" ,
+					"Name of the MCParticle collection"  ,
+					m_mcParticleCollection,
+					std::string("MCParticle")
+				);
+
+	registerInputCollection(	LCIO::RECONSTRUCTEDPARTICLE,
+					"PfoCollection",
+					"Name of input pfo collection",
+					m_inputPfoCollection,
+					std::string("PandoraPFOs")
+				);
+
+	registerInputCollection(	LCIO::RECONSTRUCTEDPARTICLE,
+					"JetCollection",
+					"Name of input jet collection",
+					m_inputJetCollection,
+					std::string("Durham_nJets")
+				);
+
+	registerInputCollection(	LCIO::MCPARTICLE,
+					"jetFlavour" ,
+					"Name of input Jet Flavour collection",
+					m_jetFlavour ,
+					std::string("jetFlavour")
+				);
+
+	registerInputCollection(	LCIO::VERTEX,
+					"PrimaryVertex",
+					"Name of Primary Vertex Collection",
+					m_inputPrimaryVertex,
+					std::string("PrimaryVertex")
+				);
+
+	registerInputCollection(	LCIO::VERTEX,
+					"BuildUpVertex",
+					"Name of BuildUp Vertex Collection",
+					m_inputBuildUpVertex,
+					std::string("BuildUpVertex")
+				);
+
+	registerInputCollection(	LCIO::LCRELATION,
+					"RecoMCTruthLinkCollection",
+					"Name of input RecoMCTruthLink Collection",
+					m_RecoMCTruthLinkCollection,
+					std::string("RecoMCTruthLinkCollection")
+				);
+
+	registerInputCollection(	LCIO::LCRELATION,
+					"MCTruthRecoLinkCollection",
+					"Name of input MCTruthRecoLink Collection",
+					m_MCTruthRecoLinkCollection,
+					std::string("MCTruthRecoLinkCollection")
+				);
+
+	registerInputCollection(	LCIO::LCRELATION,
+					"TrackMCTruthLinkCollection",
+					"Name of input TrackMCTruthLink Collection",
+					m_TrackMCTruthLinkCollection,
+					std::string("MarlinTrkTracksMCTruthLink")
+				);
+
+	registerInputCollection(	LCIO::LCRELATION,
+					"MCTruthTrackLinkCollection",
+					"Name of input MCTruthTrackLink Collection",
+					m_MCTruthTrackLinkCollection,
+					std::string("MCTruthMarlinTrkTracksLink")
+				);
+
+	registerInputCollection(	LCIO::LCRELATION,
+					"ClusterMCTruthLinkCollection",
+					"Name of input m_ClusterMCTruthLink Collection",
+					m_ClusterMCTruthLinkCollection,
+					std::string("ClusterMCTruthLink")
+				);
+
+	registerInputCollection(	LCIO::LCRELATION,
+					"MCTruthClusterLinkCollection",
+					"Name of input MCTruthClusterLink Collection",
+					m_MCTruthClusterLinkCollection,
+					std::string("MCTruthClusterLink")
+				);
+
+	registerOutputCollection(	LCIO::RECONSTRUCTEDPARTICLE,
+					"SLDNeutrinoCollection",
+					"Name of semi-leptonic Neutrino collection",
+					m_SLDNuCollection,
+					std::string("SLDNeutrinoCollection")
+				);
+
+}
+
+void SLDCorrection::init()
+{
+
+}
+
+void SLDCorrection::Clear()
+{
+
+}
+
+void SLDCorrection::processRunHeader()
+{
+
+}
+
+void SLDCorrection::processEvent( EVENT::LCEvent *pLCEvent )
+{
+
+}
+
+void SLDCorrection::check( EVENT::LCEvent *pLCEvent )
+{
+
+}
+
+void SLDCorrection::end()
+{
+
+}
